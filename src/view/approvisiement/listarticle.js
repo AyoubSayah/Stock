539c2848d5d147bf0cb4ea1ff7984a4121b90c52
AyoubSayah <sayahayoub9827@gmail.com>
2021-09-14 17:15:06 +0200

    message

--- src/view/approvisiement/listarticle.js
+++ src/view/approvisiement/listarticle.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import * as apis from './appro.service';
-
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import { Scale } from 'chart.js';
 const useStyle = makeStyles((theme) => ({
   principale: {},
   paper: {
     position: 'absolute',
-    left: '80%',
+    left: '50%',
     top: '50%',
     transform: 'translate(-50%,-50%)',
     width: '600px',
@@ -22,21 +27,62 @@ const useStyle = makeStyles((theme) => ({
     margin: '1rem auto',
     width: '150px',
   },
+  icon: {
+    color: '#ff5722',
+    width: '1.8rem',
+    height: '1.8rem',
+    cursor: 'pointer',
+    transition: 'all .06s',
+    '&:hover': {
+      transform: 'scale(1.03)',
+    },
+  },
 }));
-export default function Listeartcle(props) {
+export default function Listeartcle({ listearticle }) {
   const clases = useStyle();
-  const [listeart, setliste] = React.useState(0);
- 
+
   return (
     <div className={clases.principale}>
       <Paper className={clases.paper}>
-    
-        <Button
-          variant='contained'
-          color='primary'
-          className={clases.button}
-        
-        >
+        <Table className={clases.table} aria-label='simple table'>
+          <TableHead className={clases.table}>
+            <TableRow>
+              <TableCell className={clases.body}>code_article</TableCell>
+              <TableCell className={clases.body}>libelle</TableCell>
+
+              <TableCell className={clases.body}>QT Commandées</TableCell>
+              <TableCell className={clases.body}>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          {listearticle && listearticle.length > 0 && (
+            <TableBody>
+              {listearticle.map((data) => {
+                return (
+                  <TableRow key={'1'}>
+                    <TableCell
+                      className={clases.body}
+                      component='th'
+                      scope='row'
+                    >
+                      {data.code_article}
+                    </TableCell>
+                    <TableCell className={clases.body}>
+                      {data.libelle}
+                    </TableCell>
+                    <TableCell className={clases.body}>
+                      {data.quantite}
+                    </TableCell>
+                    <TableCell className={clases.body}>
+                      {' '}
+                      <DeleteForeverIcon className={clases.icon} />
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          )}
+        </Table>
+        <Button variant='contained' color='primary' className={clases.button}>
           Confirmer
         </Button>
       </Paper>
